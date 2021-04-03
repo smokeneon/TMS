86ea0a0fa8e22069829d27e35d423ef034748b05
Bohan Wang <wbhan_cn@qq.com>
2021-04-03 14:47:38 +0800

    切换机器

--- tms_manager/models.py
+++ tms_manager/models.py
@@ -1,9 +1,8 @@
 # Create your models here.
-from django.db import models
-from django.db.models import CharField, BooleanField, IntegerField, AutoField
+from django.db.models import Model, CharField, BooleanField, IntegerField, AutoField, ForeignKey, CASCADE
 
 
-class Course(models.Model):
+class Course(Model):
     courseId = AutoField(primary_key=True)
     courseName = CharField(max_length=255, unique=True, null=False)
     subject = CharField(max_length=255, unique=False, null=False)
@@ -12,3 +11,11 @@ class Course(models.Model):
     courseFramework = CharField(max_length=255, unique=False, null=False)
     openState = BooleanField(default=True)
     approvalState = IntegerField(default=0)
+
+
+class Apply(Model):
+    course = ForeignKey(Course, on_delete=CASCADE, to_fields="course_id")
+
+
+if __name__ == '__main__':
+    pass
